4b9e489e60d8250a57b8e089a54e2c6948818952
pameeee <65885430+pameeee@users.noreply.github.com>
2023-09-05 14:00:57 +0800

    Add event listener to buttons

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function add(x, y) {
+    console.log(x + y);
     return x + y
 }
+//
+let input = [];
 
-console.log(add(5, 3));
+var buttons = document.querySelectorAll("button");
+buttons.forEach(
+    function(button) {
+        button.addEventListener("click", function() {
+            input.push(!button.id ? parseFloat(button.innerHTML) : button.innerHTML);
+            console.log("Input so far: ", input);
+        });
+    }
+);
+
