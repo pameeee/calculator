cac303305eabfa6114d1c2b25af2b22d6a3655f0
pameeee <65885430+pameeee@users.noreply.github.com>
2023-09-05 19:34:24 +0800

    Add * and / functions, made functional, only up to digit 5

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-function add(x, y) {
-    console.log(x + y);
-    return x + y
-}
-//
-let input = [];
+function Calculate() {
+    this.methods = {
+        "+": function (firstNum, secondNum) {
+            return firstNum + secondNum
+        },
 
-var buttons = document.querySelectorAll("button");
-buttons.forEach(
-    function(button) {
-        button.addEventListener("click", function() {
-            input.push(!button.id ? parseFloat(button.innerHTML) : button.innerHTML);
-            console.log("Input so far: ", input);
-        });
+        "-": function (firstNum, secondNum) {
+            return firstNum - secondNum
+        },
+
+        "*": function (firstNum, secondNum) {
+            return firstNum * secondNum
+        },
+
+        "/": function (firstNum, secondNum) {
+            return firstNum / secondNum
+        },
+    };
+
+    this.getResult = function (firstNum, secondNum, op) {
+        const result = this.methods[op](firstNum, secondNum);
+        input = [result]
+        console.log(result);
+        console.log("Input so far: ", input);
     }
-);
 
+}
+
+let input = [];
+const calculator = new Calculate();
+// change to switch case
+var buttons = document.querySelectorAll("button");
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
+        if (button.id === "equal") {
+            let firstNum = input[0];
+            let secondNum = input[2];
+            let op = input[1];
+
+            calculator.getResult(firstNum, secondNum, op);
+        } else if (isNaN(button.innerHTML)) {
+            input.push(button.innerHTML.toString());
+        } else{
+            input.push(parseInt(button.innerHTML));
+        }
+        console.log("Input so far: ", input);
+    });
+});
