99d8d1eb672cce8d61b5b78a4a92cb62bbb0e720
pameeee <65885430+pameeee@users.noreply.github.com>
2023-09-05 21:58:01 +0800

    Added event listeners to digit buttons

--- script.js
+++ script.js
@@ -19,30 +19,28 @@ function Calculate() {
 
     this.getResult = function (firstNum, secondNum, op) {
         const result = this.methods[op](firstNum, secondNum);
-        input = [result]
-        console.log(result);
+        input = [result];
         console.log("Input so far: ", input);
     }
 
 }
 
-let input = [];
-const calculator = new Calculate();
-// change to switch case
-var buttons = document.querySelectorAll("button");
-buttons.forEach(function (button) {
-    button.addEventListener("click", function () {
-        if (button.id === "equal") {
-            let firstNum = input[0];
-            let secondNum = input[2];
-            let op = input[1];
+let input = ["", ""];
+let inputIndex = 0;
+let operator = "";
 
-            calculator.getResult(firstNum, secondNum, op);
-        } else if (isNaN(button.innerHTML)) {
-            input.push(button.innerHTML.toString());
-        } else{
-            input.push(parseInt(button.innerHTML));
-        }
-        console.log("Input so far: ", input);
-    });
-});
+const calculator = new Calculate();
+let digitButtons = document.querySelectorAll("button:not(#equal):not(.operator)");
+
+digitButtons.forEach(
+    function (button) {
+        button.addEventListener("click", function() {
+            if (inputIndex === 0) {
+                input[inputIndex] += button.innerHTML;
+                console.log("Input so far: ", input);
+            }
+        });
+    }
+);
+
+
